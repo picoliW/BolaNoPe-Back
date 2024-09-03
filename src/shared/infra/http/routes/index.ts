c2f5e6c93a7ceb89737b989af19128a3765e865f
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-03 01:26:53 -0300

    feat: add auth route to infra routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,12 @@
+import authRouter from "@modules/users/infra/http/routes/Auth.route";
 import usersRouter from "@modules/users/infra/http/routes/User.route";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use("/user", usersRouter);
+
+routes.use("/auth", authRouter);
 
 routes.get("/", (request, response) => {
   return response.json("Hello world");
