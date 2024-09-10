f5720c66bfeba947cbbeb5a3ae06fc82c3f9f33a
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-09 23:42:58 -0300

    feat: create field domain repo

---
+++ src/modules/fields/domain/repositories/IFieldRepository.ts
@@ -0,0 +1,20 @@
+import Field from "@modules/fields/infra/typeorm/entities/Field";
+import { ICreateField } from "../models/ICreateField";
+import { ObjectId } from "mongodb";
+
+export interface IFieldsRepository {
+  create({
+    name,
+    location,
+    value,
+    obs,
+    days,
+    schedules,
+    available,
+  }: ICreateField): Promise<Field>;
+  save(field: Field): Promise<Field>;
+  find(): Promise<Field[]>;
+  remove(field: Field): Promise<void>;
+  findById(id: ObjectId): Promise<Field | null>;
+  update(field: Field): Promise<Field>;
+}
