7dd2ea03adfde67c59fd6b36360736924d3a4579
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-09 23:39:22 -0300

    feat: create 'updade field' service

---
+++ src/modules/fields/services/UpdateFieldService.ts
@@ -0,0 +1,69 @@
+import "reflect-metadata";
+import { inject, injectable } from "tsyringe";
+import { ObjectId } from "mongodb";
+import { IFieldsRepository } from "../domain/repositories/IFieldRepository";
+import Field from "../infra/typeorm/entities/Field";
+import { NotFoundError } from "@shared/errors/NotFoundError";
+import { IUpdateField } from "../domain/models/IUpdateField";
+
+@injectable()
+class UpdateFieldService {
+  constructor(
+    @inject("FieldsRepository")
+    private fieldRepository: IFieldsRepository,
+  ) {}
+
+  public async execute({
+    _id,
+    name,
+    location,
+    value,
+    obs,
+    days,
+    schedules,
+    available,
+  }: IUpdateField): Promise<Partial<Field>> {
+    const field = await this.fieldRepository.findById(new ObjectId(_id));
+
+    if (!field) {
+      throw new NotFoundError("Field not found");
+    }
+
+    const updatedFields: Partial<Field> = {};
+
+    if (name) {
+      field.name = name;
+      updatedFields.name = name;
+    }
+    if (location) {
+      field.location = location;
+      updatedFields.location = location;
+    }
+    if (value) {
+      field.value = value;
+      updatedFields.value = value;
+    }
+    if (obs) {
+      field.obs = obs;
+      updatedFields.obs = obs;
+    }
+    if (days) {
+      field.days = days;
+      updatedFields.days = days;
+    }
+    if (schedules) {
+      field.schedules = schedules;
+      updatedFields.schedules = schedules;
+    }
+    if (available) {
+      field.available = available;
+      updatedFields.available = available;
+    }
+
+    await this.fieldRepository.save(field);
+
+    return updatedFields;
+  }
+}
+
+export default UpdateFieldService;
