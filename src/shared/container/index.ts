2a6157aecbb09897b6affaf061c481df0631832a
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-09 23:39:01 -0300

    feat: add fields repository to tsyringe injector

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { IFieldsRepository } from "@modules/fields/domain/repositories/IFieldRepository";
+import FieldsRepository from "@modules/fields/infra/typeorm/repositories/FieldRepository";
 import { IUsersRepository } from "@modules/users/domain/repositories/IUsersRepository";
 import UsersRepository from "@modules/users/infra/typeorm/repositories/UserRepository";
 import { container } from "tsyringe";
@@ -5,4 +7,9 @@ import { container } from "tsyringe";
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository,
+);
+
+container.registerSingleton<IFieldsRepository>(
+  "FieldsRepository",
+  FieldsRepository,
 );
