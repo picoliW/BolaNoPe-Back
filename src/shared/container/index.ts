092eee3e922cfc90ab64f98d2130a37ebb8c52a4
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-11 02:47:26 -0300

    feat: add reserve repsitory to tsyringe injector

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { IFieldsRepository } from "@modules/fields/domain/repositories/IFieldRepository";
 import FieldsRepository from "@modules/fields/infra/typeorm/repositories/FieldRepository";
+import { IReserveRepository } from "@modules/reserves/domain/repositories/IReserveRepository";
+import ReserveRepository from "@modules/reserves/infra/typeorm/repositories/ReserveRepository";
 import { IUsersRepository } from "@modules/users/domain/repositories/IUsersRepository";
 import UsersRepository from "@modules/users/infra/typeorm/repositories/UserRepository";
 import { container } from "tsyringe";
@@ -12,4 +14,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IFieldsRepository>(
   "FieldsRepository",
   FieldsRepository,
+);
+
+container.registerSingleton<IReserveRepository>(
+  "ReservesRepository",
+  ReserveRepository,
 );
