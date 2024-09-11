7d644e8ee3fa31f9b7c613e592177a1a365ad87b
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-11 02:55:24 -0300

    feat: create reserve domain repo

---
+++ src/modules/reserves/domain/repositories/IReserveRepository.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+import { ICreateReserve } from "../models/ICreateReserve";
+import Reserve from "@modules/reserves/infra/typeorm/entities/Reserve";
+
+export interface IReserveRepository {
+  create({
+    id_user,
+    start_hour,
+    end_hour,
+    id_field,
+    final_value,
+  }: ICreateReserve): Promise<Reserve>;
+  save(reserve: Reserve): Promise<Reserve>;
+  find(): Promise<Reserve[]>;
+  findById(id: ObjectId): Promise<Reserve | null>;
+  update(reserve: Reserve): Promise<Reserve>;
+  remove(reserve: Reserve): Promise<void>;
+}
