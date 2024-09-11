7f6fd3531baf0e4fae81b3fd3d94140f686e4625
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-11 02:54:50 -0300

    feat: create reserve routes

---
+++ src/modules/reserves/infra/http/routes/reserve.route.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { container } from "tsyringe";
+import ReserveController from "../controller/ReserveController";
+import { validateCreateReserve } from "../../schemas/CreateReserveValidator";
+import { validateObjectIdMIddleware } from "@shared/infra/http/middlewares/ValidateObjectIdMiddleware";
+import { validateUpdateReserve } from "../../schemas/UpdateReserveValidator";
+
+const reserveRouter = Router();
+const reserveController = container.resolve(ReserveController);
+
+reserveRouter.post("/", validateCreateReserve, reserveController.create);
+
+reserveRouter.get("/", reserveController.index);
+
+reserveRouter.get("/:id", validateObjectIdMIddleware, reserveController.show);
+
+reserveRouter.put(
+  "/:id",
+  validateUpdateReserve,
+  validateObjectIdMIddleware,
+  reserveController.update,
+);
+
+reserveRouter.delete(
+  "/:id",
+  validateObjectIdMIddleware,
+  reserveController.delete,
+);
+
+export default reserveRouter;
