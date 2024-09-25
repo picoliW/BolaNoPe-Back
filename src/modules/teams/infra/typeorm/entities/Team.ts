62af8db45752f295a2a738c53097295cac71b88a
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-25 00:17:44 -0300

    feat: create team entity

---
+++ src/modules/teams/infra/typeorm/entities/Team.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ObjectIdColumn } from "typeorm";
+import { ObjectId } from "mongodb";
+
+@Entity("Teams")
+class Team {
+  @ObjectIdColumn()
+  _id: ObjectId;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  leader_id: string;
+
+  @Column()
+  members_id: string[];
+
+  @Column()
+  tourneys_id: string[];
+}
+
+export default Team;
