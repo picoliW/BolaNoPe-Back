82552ebe64d87afa616e3e80e9df1542e948d6f2
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-09-25 00:18:29 -0300

    feat: create teams controller

---
+++ src/modules/teams/infra/http/controller/TeamController.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ObjectId } from "mongodb";
+import { ConflictError } from "@shared/errors/ConflictError";
+import { NotFoundError } from "@shared/errors/NotFoundError";
+import { BadRequestError } from "@shared/errors/BadRequestError";
+import ListTeamService from "@modules/teams/services/ListTeamService";
+import CreateTeamService from "@modules/teams/services/CreateTeamService";
+import DeleteTeamService from "@modules/teams/services/DeleteTeamService";
+import ShowOneTeamService from "@modules/teams/services/ShowOneTeamService";
+import UpdateTeamService from "@modules/teams/services/UpdateTeamService";
+
+export default class TeamsController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listTeam = container.resolve(ListTeamService);
+
+    const teams = await listTeam.execute();
+
+    return res.json(teams);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, description, leader_id, members_id, tourneys_id } =
+      request.body;
+
+    const createTeam = container.resolve(CreateTeamService);
+
+    try {
+      const team = await createTeam.execute({
+        name,
+        description,
+        leader_id,
+        members_id,
+        tourneys_id,
+      });
+
+      return response.status(201).json({ team });
+    } catch (error) {
+      if (error instanceof ConflictError) {
+        return response.status(409).json({ message: error.message });
+      } else if (error instanceof BadRequestError) {
+        return response.status(400).json({ message: error.message });
+      }
+      throw error;
+    }
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const deleteTeam = container.resolve(DeleteTeamService);
+
+    try {
+      const objectId = new ObjectId(id);
+      await deleteTeam.execute({ _id: objectId });
+      return res.status(204).json();
+    } catch (error) {
+      return res.status(404).json({ message: "Team not found" });
+    }
+  }
+
+  public async show(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const showTeam = container.resolve(ShowOneTeamService);
+    const objectId = new ObjectId(id);
+    const team = await showTeam.execute(objectId);
+
+    if (!team) {
+      return res.status(404).json({ error: "Tourney not found" });
+    }
+
+    return res.json(team);
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { name, description, leader_id, members_id, tourneys_id } = req.body;
+
+    const updateTeamService = container.resolve(UpdateTeamService);
+    const objectId = new ObjectId(id);
+
+    try {
+      const updatedTeams = await updateTeamService.execute({
+        _id: objectId,
+        name,
+        description,
+        leader_id,
+        members_id,
+        tourneys_id,
+      });
+
+      return res.status(200).json(updatedTeams);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      } else if (error instanceof ConflictError) {
+        return res.status(409).json({ message: error.message });
+      } else if (error instanceof BadRequestError) {
+        return res.status(400).json({ message: error.message });
+      }
+      throw error;
+    }
+  }
+}
