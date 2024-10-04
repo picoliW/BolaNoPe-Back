4a6683a2a66922bcaa654f6978d47e15cebd2cab
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-10-04 11:54:50 -0300

    fix: fix error return

--- src/modules/teams/infra/http/controller/TeamRequestController.ts
+++ src/modules/teams/infra/http/controller/TeamRequestController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 import HandleTeamRequestService from "@modules/teams/services/HandleTeamRequestService";
 import CreateTeamRequestService from "@modules/teams/services/CreateTemRequestService";
+import { NotFoundError } from "@shared/errors/NotFoundError";
+import { ConflictError } from "@shared/errors/ConflictError";
+import { BadRequestError } from "@shared/errors/BadRequestError";
 
 export default class TeamRequestController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -12,9 +15,17 @@ export default class TeamRequestController {
       CreateTeamRequestService,
     );
 
-    const request = await createTeamRequestService.execute(teamId, userId);
-
-    return res.status(201).json(request);
+    try {
+      const request = await createTeamRequestService.execute(teamId, userId);
+      return res.status(201).json(request);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      } else if (error instanceof BadRequestError) {
+        return res.status(400).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 
   public async handle(req: Request, res: Response): Promise<Response> {
@@ -25,13 +36,22 @@ export default class TeamRequestController {
     const handleTeamRequestService = container.resolve(
       HandleTeamRequestService,
     );
-
-    const request = await handleTeamRequestService.execute(
-      requestId,
-      leaderId,
-      action,
-    );
-
-    return res.status(200).json(request);
+    try {
+      const request = await handleTeamRequestService.execute(
+        requestId,
+        leaderId,
+        action,
+      );
+      return res.status(200).json(request);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      } else if (error instanceof ConflictError) {
+        return res.status(409).json({ message: error.message });
+      } else if (error instanceof BadRequestError) {
+        return res.status(400).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 }
